043131313836038ed0a378fb4a75c31ecbb314c4
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2025-09-15 11:24:40 +0300

    Added new features

---
+++ main.js
@@ -0,0 +1,11 @@
+const ActionButon = document.querySelector('#Action');
+const checkedButton = document.querySelector("#buttonChecked")
+
+
+ActionButon.addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    const CheckedMessage = checkedButton
+
+    CheckedMessage.textContent = "Chencked"
+})
